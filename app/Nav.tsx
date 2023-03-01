84f6141b76c65798f6af3051f198dd83253851a6
spatulatom <spatulatom@gmail.com>
2023-03-01 19:39:41 +0000

    prettier

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 import { unstable_getServerSession } from 'next-auth/next';
 // import { authOptions } from '../pages/api/auth/[...nextauth]';
 import { signIn } from 'next-auth/react';
@@ -6,31 +6,35 @@ import Login from './Login';
 import Logged from './Logged';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { useSession } from "next-auth/react";
-
+import { useSession } from 'next-auth/react';
 
 export default function Nav() {
   // const session = await unstable_getServerSession(authOptions);
-  const router = useRouter()
+  const router = useRouter();
   const { data } = useSession();
 
   return (
     <nav className="flex justify-between items-center py-8 ">
-      <span onClick={()=>router.push('/')}>
-        <h1 className="cursor-pointer font-bold text-teal-600 hover:text-teal-700 transition-all md:text-2xl italic">Chat Room</h1>
+      <span onClick={() => router.push('/')}>
+        <h1 className="cursor-pointer font-bold text-teal-600 hover:text-teal-700 transition-all md:text-2xl italic">
+          Chat Room
+        </h1>
       </span>
       <ul className="flex items-center gap-2 md:gap-6">
-      <li>
-            <h1 onClick={()=>router.push('/')}>
-              <h1 className="hover:text-teal-600 transition-all md:text-lg cursor-pointer">Home</h1>
+        <li>
+          <h1 onClick={() => router.push('/')}>
+            <h1 className="hover:text-teal-600 transition-all md:text-lg cursor-pointer">
+              Home
             </h1>
-          </li>
-        
-      
+          </h1>
+        </li>
+
         {data && (
           <li>
             <Link href={'/myposts'}>
-              <h1 className="hover:text-teal-600 transition-all md:text-lg">My Chats</h1>
+              <h1 className="hover:text-teal-600 transition-all md:text-lg">
+                My Chats
+              </h1>
             </Link>
           </li>
         )}
